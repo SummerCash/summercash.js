bc7125f99c9650ff651b0fb80d42e2bc582a8dff
Dowland Aiello <mitsukomegumii@gmail.com>
2019-03-31 17:02:36 -0400

    src: Finished accounts api.

--- src/AccountsAPI.ts
+++ src/AccountsAPI.ts
@@ -22,4 +22,30 @@ export class AccountsAPI {
 
     return request.post(options); // Return request response
   }
+
+  // getAllAccounts calls the GetAllAccounts() RPC method.
+  public getAllAccounts(): request.RequestPromise<any> {
+    const options = {
+      method: 'POST', // Set method
+      uri: `${this.serviceLookup}/GetAllAccounts`, // Set URI
+      body: {}, // Init empty body
+      json: true, // Set JSON
+    }; // Init options
+
+    return request.post(options); // Return request response
+  }
+
+  // string calls the string() rpc method.
+  public string(accountAddress: string): request.RequestPromise<any> {
+    const options = {
+      method: 'POST', // Set method
+      uri: `${this.serviceLookup}/GetAllAccounts`, // Set URI
+      body: {
+        address: accountAddress, // Set account address
+      }, // Init empty body
+      json: true, // Set JSON
+    }; // Init options
+
+    return request.post(options); // Return request response
+  }
 }
