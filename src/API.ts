997c5da6bef8897141fae72e0c761797ba5585e1
Dowland Aiello <mitsukomegumii@gmail.com>
2019-03-31 16:59:19 -0400

    src: Init accounts API on constructor call.

--- src/API.ts
+++ src/API.ts
@@ -1,9 +1,15 @@
+import { AccountsAPI } from './AccountsAPI';
+
 // Class API defines a SummerCash API instance.
 export class API {
+  public accounts: AccountsAPI; // Init accounts API buffer
+
   public provider: string; // Init provider buffer
 
   // Initializes a new API instance with a given provider.
-  constructor(provider: string) {
+  public constructor(provider: string) {
     this.provider = provider; // Set provider
+
+    this.accounts = new AccountsAPI(provider); // Initialize accounts API
   }
 }
